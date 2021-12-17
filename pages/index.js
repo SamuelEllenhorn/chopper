8cb64a8724624bcd0017d8685a04fac080911431
Samuel Ellenhorn <ellenhorn@chapman.edu>
2021-12-16 16:19:44 -0800

    com

--- pages/index.js
+++ pages/index.js
@@ -5,72 +5,144 @@ import { Auth } from '@supabase/ui'
 import React, { useEffect, useState } from "react";
 import { useItems } from '../context/ItemContext'
 import { useUser } from '../context/UserContext' 
+import ItemCard from '../components/ItemCard'
 
-export default function Home() {
 
-  const { shopItems } = useItems()
+
+export default function Home() 
+{
+
+  
+  const { items } = useItems()
   return (
-<div class="wrapper">  
-  <header>
-    <div class="topnav">
-            <a class="motomf" href="#motomf">motomf</a>       
-    </div>
-  </header>
 
-  <div class = "navBar" >
+
+
+<div class = "outerWraper">
+  
+
+
+
+
+
+
+<div class = "topWraper">
+
+
+  <div class="topnav">
+    <header>
+      <a class="motomf" href="#motomf">MotoMf</a>      
+    </header> 
+
+    <div class = "icon">
+     
+    </div>
+  </div>
+
+
+
+
+<div class = "navBar" >
       <ul>
       <li><a href="default.asp">Home</a></li>
       <li><a href="news.asp">News</a></li>
-      <li><a href="contact.asp">Contact</a></li>
+      <li><a href="Contact.asp">Contact</a></li>
       <li><a href="about.asp">About</a></li>
-      <li> cart
-        
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+
+      <li> 
+        <div class = "cartWraper">
+        <a href="cart.asp"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-        </svg>
-        
+        </svg>ss</a>
+        </div>
         </li>
+
+      
+
+
+
       </ul>
-    </div>
+</div>
+
+</div>
+
+
+<div class="wrapper">  
 
   <div class = "bikeIMG">
-    <img src="\motionPic.jpg"  alt="Girl in a jacket" width="100%" height="100%"></img>
-    
+        <div class="mottoSpacer">
+            
+        </div>
+    <div class="first-txt">
+            Win the Bike of your Dreams
+        </div>
   </div>
 
   <div class = "missionStatement">
      <p>
-       We are selling this bike for charity!
+       We are selling this bike for charity! The specs of this bike are as follows, for information on 
+       how the auction works pleaser read below:
+
+       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+        The auction will be held on... 
      </p>
       
     </div>
 
+    <div className="iframe-container">
+    
+    <div className = "videoText">
+    live the experience...
+    </div>
+    
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ivlafsmBwxo" title="YouTube video player" 
+      allowfullscreen></iframe>
+    </div>
+   
 
 <div class = "shop">
-
-  {/* TODO: Map through the items in context 
-          * to display an ItemCard with the data for each
-          */
-  /*
-          shopItems.map((item, i) => (
+  {
+          items.map((item, i) => (
             <ItemCard 
             key={i}
             name={item.name}
             img={item.img}
             stock={item.stock}
             price={item.price}
-            add={addToCart}
+            //add={addToCart}
             />
-          ))*/
-          }
+          ))
+  }
+
+
+
+
+
  
 
 </div>
 
+
+
+<div class = "contact">
+     <div class = "contactInfo">
+      Contact: 310-424-497
+     
+     </div>
+
+     <div class = "icons">
+       
+     <a href="#" class="fa fa-facebook"></a>
+     <a href="#" class="fa fa-twitter"></a>
+     <a href="#" class="fa fa-instagram"></a>
+      </div>
+    </div>
+
   </div>
 
 
-
+  </div>
 
 
  
