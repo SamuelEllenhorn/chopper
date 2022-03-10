4085f344bd2a8ee39e59f9f19709260465e6d921
Samuel Ellenhorn <ellenhorn@chapman.edu>
2022-03-09 16:10:26 -0800

    changed \cart to home page

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export default function Home()
           <div class = "liWraper">
           <li> 
             <div class = "cartWraper">
-            <a href="/cart">
+            <a href="https://www.motomf.com/">
             <div class = "cart">
 
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
